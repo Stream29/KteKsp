234d102d355c18dc973356a3e05007c008b78f48
ConstasJ <constasj@qq.com>
2025-05-06 09:34:26 +0800

    chore: add Kotlin JVM maven publish convention plugin

---
+++ buildSrc/src/main/kotlin/publish-convention-jvm.gradle.kts
@@ -0,0 +1,44 @@
+import com.vanniktech.maven.publish.*
+
+plugins {
+    alias(libs.plugins.maven.publish)
+}
+
+mavenPublishing {
+    configure(KotlinJvm(
+        javadocJar = JavadocJar.Empty(),
+        sourcesJar = true
+    ))
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+    pom {
+        name.set("KteKsp")
+        description.set("Kotlin template engine for KSP")
+        inceptionYear.set("2025")
+        url.set("https://github.com/stream29/KteKsp")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("Stream29")
+                name.set("Stream")
+                url.set("https://github.com/Stream29")
+            }
+            developer {
+                id.set("ConstasJ")
+                name.set("ConstasJ")
+                url.set("https://github.com/ConstasJ")
+            }
+        }
+        scm {
+            url.set("https://github.com/Stream29/KteKsp")
+            connection.set("scm:git:git://github.com/Stream29/KteKsp.git")
+            developerConnection.set("scm:git:ssh://git@github.com/Stream29/KteKsp.git")
+        }
+    }
+}
